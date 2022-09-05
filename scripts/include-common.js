5971a1c3e7e9826defb089caddf76d89c252e21e
Samuel Do <samdo@stanford.edu>
2022-09-04 21:59:29 -0700

    Migrate to relative references for common include

--- scripts/include-common.js
+++ scripts/include-common.js
@@ -1,16 +1,16 @@
 // Dependencies: JQuery
 
 // Load common head
-$.get("/common/head.html", function(head) {
+$.get("../common/head.html", function(head) {
     $('head').prepend(head)
 });
 
 // Load common nav
-$.get("/common/nav.html", function(nav) {
+$.get("../common/nav.html", function(nav) {
     $('body').prepend(nav)
 });
 
 // Load common footer
-$.get("/common/footer.html", function(footer) {
+$.get("../common/footer.html", function(footer) {
     $('body').append(footer)
 });
